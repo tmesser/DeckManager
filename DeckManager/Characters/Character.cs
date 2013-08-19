b66c9fcae425ff4f905fa32ed9266747c05b757e
clanpotshake <kevinpkerr@gmail.com>
2013-08-19 14:36:02 -0400

    Added Titles enum, initial Character object stubs.

---
+++ DeckManager/Characters/Character.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeckManager.Cards.Enums;
+using DeckManager.Cards;
+
+namespace DeckManager.DeckManager.Characters
+{
+    class Character
+    {
+        /// <summary>
+        /// Set of draw colors. Each inner list represents mixed draws.
+        /// </summary>
+        public List<List<SkillCardColor>> drawColors { get; set; }
+
+        /// <summary>
+        /// Skill card hand
+        /// </summary>
+        public List<SkillCard> skillHand { get; private set; }
+
+        public List<Titles> titles { get; set; }
+
+        public string characterName;
+        public string playerName;
+
+
+    }
+}
---
+++ DeckManager/Characters/Enums/Titles.cs
@@ -0,0 +1,12 @@
+﻿namespace DeckManager.Cards.Enums
+{
+    public enum Titles
+    {
+        Unknown,
+        None,
+        Admiral,
+        President,
+        CAG,
+        CurrentPlayer
+    }
+}
