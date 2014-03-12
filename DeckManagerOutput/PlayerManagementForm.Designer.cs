570aab041135c936c4edfd4f99f481d680dc12b0
tmesser <timothy.messer@gmail.com>
2014-03-12 16:38:39 -0500

    Add in another quick UI element

--- DeckManagerOutput/PlayerManagementForm.Designer.cs
+++ DeckManagerOutput/PlayerManagementForm.Designer.cs
@@ -29,10 +29,13 @@
         private void InitializeComponent()
         {
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.PlayerNameLabel = new System.Windows.Forms.Label();
             this.CharacterNameLabel = new System.Windows.Forms.Label();
+            this.PlayerNameLabel = new System.Windows.Forms.Label();
             this.PlayerCardListBox = new System.Windows.Forms.ListBox();
             this.DiscardSelectedCheckBox = new System.Windows.Forms.CheckBox();
+            this.drawCardComboControl1 = new DeckManagerOutput.CustomControls.DrawCardComboControl();
+            this.LocationMarkerLabel = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,27 +48,21 @@
             this.tableLayoutPanel1.Controls.Add(this.PlayerNameLabel, 0, 0);
             this.tableLayoutPanel1.Controls.Add(this.PlayerCardListBox, 0, 1);
             this.tableLayoutPanel1.Controls.Add(this.DiscardSelectedCheckBox, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.drawCardComboControl1, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.LocationMarkerLabel, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 3);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
             this.tableLayoutPanel1.RowCount = 6;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 8.530806F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 17.45283F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 74.0566F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 26F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 18F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 16F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(292, 273);
             this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // PlayerNameLabel
-            // 
-            this.PlayerNameLabel.AutoSize = true;
-            this.PlayerNameLabel.Location = new System.Drawing.Point(3, 0);
-            this.PlayerNameLabel.Name = "PlayerNameLabel";
-            this.PlayerNameLabel.Size = new System.Drawing.Size(73, 13);
-            this.PlayerNameLabel.TabIndex = 0;
-            this.PlayerNameLabel.Text = "(Player Name)";
             // 
             // CharacterNameLabel
             // 
@@ -76,24 +73,60 @@
             this.CharacterNameLabel.TabIndex = 1;
             this.CharacterNameLabel.Text = "(Character Name)";
             // 
+            // PlayerNameLabel
+            // 
+            this.PlayerNameLabel.AutoSize = true;
+            this.PlayerNameLabel.Location = new System.Drawing.Point(3, 0);
+            this.PlayerNameLabel.Name = "PlayerNameLabel";
+            this.PlayerNameLabel.Size = new System.Drawing.Size(73, 13);
+            this.PlayerNameLabel.TabIndex = 0;
+            this.PlayerNameLabel.Text = "(Player Name)";
+            // 
             // PlayerCardListBox
             // 
             this.PlayerCardListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PlayerCardListBox.Dock = System.Windows.Forms.DockStyle.Fill;
             this.PlayerCardListBox.FormattingEnabled = true;
-            this.PlayerCardListBox.Location = new System.Drawing.Point(3, 73);
+            this.PlayerCardListBox.Location = new System.Drawing.Point(3, 21);
             this.PlayerCardListBox.Name = "PlayerCardListBox";
-            this.PlayerCardListBox.Size = new System.Drawing.Size(120, 54);
+            this.tableLayoutPanel1.SetRowSpan(this.PlayerCardListBox, 2);
+            this.PlayerCardListBox.Size = new System.Drawing.Size(143, 188);
             this.PlayerCardListBox.TabIndex = 2;
             // 
             // DiscardSelectedCheckBox
             // 
             this.DiscardSelectedCheckBox.AutoSize = true;
-            this.DiscardSelectedCheckBox.Location = new System.Drawing.Point(152, 73);
+            this.DiscardSelectedCheckBox.Location = new System.Drawing.Point(152, 21);
             this.DiscardSelectedCheckBox.Name = "DiscardSelectedCheckBox";
             this.DiscardSelectedCheckBox.Size = new System.Drawing.Size(107, 17);
             this.DiscardSelectedCheckBox.TabIndex = 3;
             this.DiscardSelectedCheckBox.Text = "Discard Selected";
             this.DiscardSelectedCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // drawCardComboControl1
+            // 
+            this.drawCardComboControl1.Location = new System.Drawing.Point(152, 58);
+            this.drawCardComboControl1.Name = "drawCardComboControl1";
+            this.drawCardComboControl1.Size = new System.Drawing.Size(137, 50);
+            this.drawCardComboControl1.TabIndex = 4;
+            // 
+            // LocationMarkerLabel
+            // 
+            this.LocationMarkerLabel.AutoSize = true;
+            this.LocationMarkerLabel.Location = new System.Drawing.Point(3, 212);
+            this.LocationMarkerLabel.Name = "LocationMarkerLabel";
+            this.LocationMarkerLabel.Size = new System.Drawing.Size(91, 13);
+            this.LocationMarkerLabel.TabIndex = 5;
+            this.LocationMarkerLabel.Text = "Currrent Location:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(152, 215);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(137, 21);
+            this.comboBox1.TabIndex = 6;
             // 
             // PlayerManagementForm
             // 
@@ -116,5 +149,8 @@
         private System.Windows.Forms.Label PlayerNameLabel;
         private System.Windows.Forms.ListBox PlayerCardListBox;
         private System.Windows.Forms.CheckBox DiscardSelectedCheckBox;
+        private CustomControls.DrawCardComboControl drawCardComboControl1;
+        private System.Windows.Forms.Label LocationMarkerLabel;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
