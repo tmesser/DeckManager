a380bcd49c8ec0a5c807f38cbaddeb204d589181
clanpotshake <kevinpkerr@gmail.com>
2014-09-13 16:36:54 -0400

    added damaged field to location and tostring method

--- DeckManager/Boards/BasicLocation.cs
+++ DeckManager/Boards/BasicLocation.cs
@@ -14,6 +14,11 @@ namespace DeckManager.Boards
         public string LocationInfo { get; set; }
 
         /// <summary>
+        /// Gets or sets whether the location has been damaged. Damaged locations cannot be activated until they are repaired.
+        /// </summary>
+        public bool Damaged { get; set; }
+
+        /// <summary>
         /// Gets or sets the name of the board the location is on.
         /// </summary>
         /// <value>
@@ -33,7 +38,7 @@ namespace DeckManager.Boards
                     players.Append(',');
                     players.Append(' ');
                 }
-                ret = string.Format("{0} - {1}", Name, players.ToString().Trim(',',' '));
+                ret = string.Format("{0} - {1}{2}", Name, players.ToString().Trim(',',' '),Damaged ? " [DAMAGED]" : "");
             }
             return ret;
         }
